14e102cae06292efe67ce395ff5488462b4e1e58
Facundo Giuliani <facundo.giuliani@gmail.com>
2019-09-18 19:49:40 -0300

    config hidden

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: "lawg6TdWMxTzPQWReYKyOT67c",
-  consumer_secret: "nJw6XhvaG8kM2tq6DSq82iRNR7LlAhaAFS1LwoHKaxRHjfio2B",
-  access_token: "1170884876708528128-qm625eXchDPGAlgiwMvVPB3aydFeA6",
-  access_token_secret: "rhrbG4za3pU4s7fdjiRV55dRimcf2qclkHbE9scqJgehZ"
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
